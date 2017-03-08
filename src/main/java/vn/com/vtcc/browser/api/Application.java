122e141d752e9d09431a17b3df2bd60d3d173cc3
Nguyen Truong Giang <giangnt83@cyberspace.vn>
2017-03-08 17:55:48 +0700

    [API] Update ES config

--- src/main/java/vn/com/vtcc/browser/api/Application.java
+++ src/main/java/vn/com/vtcc/browser/api/Application.java
@@ -27,8 +27,8 @@ import java.util.Set;
 public class Application extends WebMvcConfigurerAdapter {
 	public static final int RESPONE_STATAUS_OK = 200;
 	public static String ES_SERVER = System.getProperty("es_server");
-	//public static final String URL_ELASTICSEARCH =  ES_SERVER + "/_search?";
-	public static final String URL_ELASTICSEARCH = "http://192.168.107.232:9200/br_article_v4/article/_search?";
+	public static final String URL_ELASTICSEARCH =  ES_SERVER + "/_search?";
+	//public static final String URL_ELASTICSEARCH = "http://192.168.107.232:9200/br_article_v4/article/_search?";
 	public static final String URL_GOOGLE = "https://www.googleapis.com/oauth2/v2/userinfo?access_token=";
 	public static final String STATUS_DISPLAY = "1";
 	public static final String HOST_NAME = "http://news.safenet.vn/";

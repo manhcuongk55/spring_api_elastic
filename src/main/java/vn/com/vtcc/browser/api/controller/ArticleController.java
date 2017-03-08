d5aa7c3c174724609562fff188dbb74114506c78
Nguyen Truong Giang <giangnt83@cyberspace.vn>
2017-03-08 15:17:34 +0700

    [API] Reduce fields return to client when using 3G/4G data

--- src/main/java/vn/com/vtcc/browser/api/controller/ArticleController.java
+++ src/main/java/vn/com/vtcc/browser/api/controller/ArticleController.java
@@ -1,13 +1,14 @@
 package vn.com.vtcc.browser.api.controller;
 
+import org.json.simple.JSONArray;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import vn.com.vtcc.browser.api.model.Category;
 import vn.com.vtcc.browser.api.service.ArticleService;
@@ -16,6 +17,8 @@ import vn.com.vtcc.browser.api.service.CategoryService;
 import java.net.UnknownHostException;
 import java.sql.Timestamp;
 import java.util.List;
+
+import static com.sun.corba.se.spi.activation.IIOP_CLEAR_TEXT.value;
 
 @RestController
 public class ArticleController {
@@ -34,59 +37,66 @@ public class ArticleController {
 	@RequestMapping(value = "/get_list_hot_article", method = RequestMethod.GET, produces = "application/json")
 	public String getListHotNews(@RequestParam(value = "from", defaultValue = "0") String from,
 			@RequestParam(value = "size", defaultValue = "20") String size,
-								 @RequestParam(value = "timestamp", defaultValue = "0") String timestamp)
+								 @RequestParam(value = "timestamp", defaultValue = "0") String timestamp,
+								 @RequestParam(value = "connectivity", defaultValue = "wifi") String connectivity)
 			throws org.json.simple.parser.ParseException, UnknownHostException {
-		return ArticleService.getListHotArticle(from, size, timestamp);
+		return ArticleService.getListHotArticle(from, size, timestamp, connectivity);
 	}
 	@CrossOrigin
 	@RequestMapping(value = "/get_list_article_categoryId", method = RequestMethod.GET, produces = "application/json")
 	public String getListArticlesByCategor(@RequestParam(value = "from", defaultValue = "0") String from,
 			@RequestParam(value = "size", defaultValue = "20") String size,
 			@RequestParam(value = "categoryId", defaultValue = "0") String categoryId,
-										   @RequestParam(value = "timestamp", defaultValue = "0") String timestamp)
-			throws org.json.simple.parser.ParseException {
-		return ArticleService.getListArticleByCategoryId(from, size, categoryId,timestamp);
+										   @RequestParam(value = "timestamp", defaultValue = "0") String timestamp,
+										   @RequestParam(value = "connectivity", defaultValue = "wifi") String connectivity)
+			throws org.json.simple.parser.ParseException, UnknownHostException {
+		return ArticleService.getListArticleByCategoryId(from, size, categoryId,timestamp, connectivity);
 	}
 	@CrossOrigin
 	@RequestMapping(value = "/get_list_article_categoryName", method = RequestMethod.GET, produces = "application/json")
 	public String getListArticlesByCategoryName(@RequestParam(value = "from", defaultValue = "0") String from,
 			@RequestParam(value = "size", defaultValue = "20") String size,
 			@RequestParam(value = "categoryName", defaultValue = "a") String categoryName,
-												@RequestParam(value = "timestamp", defaultValue = "0") String timestamp)
-			throws org.json.simple.parser.ParseException {
-		return ArticleService.getListArticleByCategoryName(from, size, categoryName, timestamp);
+												@RequestParam(value = "timestamp", defaultValue = "0") String timestamp,
+												@RequestParam(value = "connectivity", defaultValue = "wifi") String connectivity)
+			throws org.json.simple.parser.ParseException, UnknownHostException {
+		return ArticleService.getListArticleByCategoryName(from, size, categoryName, timestamp, connectivity);
 	}
 	@CrossOrigin
 	@RequestMapping(value = "/get_list_article_tags", method = RequestMethod.GET, produces = "application/json")
 	public String getListArticlesByTags(@RequestParam(value = "from", defaultValue = "0") String from,
 			@RequestParam(value = "size", defaultValue = "20") String size,
 			@RequestParam(value = "tags", defaultValue = "a") String tags,
-										@RequestParam(value = "timestamp", defaultValue = "0") String timestamp)
-			throws org.json.simple.parser.ParseException {
-		return ArticleService.getListArticleByTags(from, size, tags,timestamp);
+										@RequestParam(value = "timestamp", defaultValue = "0") String timestamp,
+										@RequestParam(value = "connectivity", defaultValue = "wifi") String connectivity)
+			throws org.json.simple.parser.ParseException, UnknownHostException {
+		return ArticleService.getListArticleByTags(from, size, tags,timestamp, connectivity);
 	}
 	@CrossOrigin
 	@RequestMapping(value = "/get_list_article_related_tags", method = RequestMethod.GET, produces = "application/json")
 	public String getListArticlReleatedTags(@RequestParam(value = "tags", defaultValue = "a") String tags,
-			@RequestParam(value = "number", defaultValue = "4") String number,@RequestParam(value = "timestamp", defaultValue = "0") String timestamp)
-			throws org.json.simple.parser.ParseException {
-		return ArticleService.getListArticlReleatedTags(tags, number, timestamp);
+			@RequestParam(value = "number", defaultValue = "4") String number,@RequestParam(value = "timestamp", defaultValue = "0") String timestamp,
+											@RequestParam(value = "connectivity", defaultValue = "wifi") String connectivity)
+			throws org.json.simple.parser.ParseException, UnknownHostException {
+		return ArticleService.getListArticlReleatedTags(tags, number, timestamp, connectivity);
 	}
 	@CrossOrigin
 	@RequestMapping(value = "/get_list_article_tittle", method = RequestMethod.GET, produces = "application/json")
 	public String getListArticlSearchByTitle(@RequestParam(value = "from", defaultValue = "0") String from,
 			@RequestParam(value = "size", defaultValue = "20") String size,
-			@RequestParam(value = "title", defaultValue = "title") String title)
-			throws org.json.simple.parser.ParseException {
-		return ArticleService.getListArticleByStringInTitle(from, size, title);
+			@RequestParam(value = "title", defaultValue = "title") String title,
+											 @RequestParam(value = "connectivity", defaultValue = "wifi") String connectivity)
+			throws org.json.simple.parser.ParseException, UnknownHostException {
+		return ArticleService.getListArticleByStringInTitle(from, size, title, connectivity);
 	}
 	@CrossOrigin
 	@RequestMapping(value = "/get_list_article_source", method = RequestMethod.GET, produces = "application/json")
 	public String getListArticlSearchBySource(@RequestParam(value = "from", defaultValue = "0") String from,
 			@RequestParam(value = "size", defaultValue = "20") String size,
-			@RequestParam(value = "source", defaultValue = "source") String source,@RequestParam(value = "timestamp", defaultValue = "0") String timestamp)
-			throws org.json.simple.parser.ParseException {
-		return ArticleService.getListArticleByStringInSource(from, size, source,timestamp);
+			@RequestParam(value = "source", defaultValue = "source") String source,@RequestParam(value = "timestamp", defaultValue = "0") String timestamp,
+											  @RequestParam(value = "connectivity", defaultValue = "wifi") String connectivity)
+			throws org.json.simple.parser.ParseException, UnknownHostException {
+		return ArticleService.getListArticleByStringInSource(from, size, source,timestamp, connectivity);
 	}
 
 	@CrossOrigin
@@ -97,8 +107,15 @@ public class ArticleController {
 	}
 	@CrossOrigin
 	@RequestMapping(value = "/get_list_hot_tags", method = RequestMethod.GET, produces = "application/json")
-	public String getListHotTags()
+	public ResponseEntity<Object> getListHotTags()
 			throws org.json.simple.parser.ParseException {
 		return ArticleService.getListHotTags();
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = "/update_hot_tags", method = RequestMethod.POST)
+	public ResponseEntity<Object> updateRedisHotTags(@RequestBody String input)
+			throws org.json.simple.parser.ParseException {
+		return ArticleService.updateRedisHotTags(input);
 	}
 }
--- src/main/java/vn/com/vtcc/browser/api/service/ArticleService.java
+++ src/main/java/vn/com/vtcc/browser/api/service/ArticleService.java
@@ -5,6 +5,7 @@ import java.net.InetAddress;
 import java.net.URLEncoder;
 import java.net.UnknownHostException;
 import java.sql.Timestamp;
+import javax.annotation.Resource;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -18,28 +19,42 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import redis.clients.jedis.Jedis;
 import vn.com.vtcc.browser.api.Application;
 import vn.com.vtcc.browser.api.exception.DataNotFoundException;
+import vn.com.vtcc.browser.api.unitTest.UpdateRedisUnitTest;
 
 public class ArticleService {
 	private static final int TIMESTAMP_DAY_BEFORE = 86400000;
 	private static final int CONNECTION_TIMEOUT = 1000;
+	private Jedis jedis = new Jedis("localhost");
+	private String ES_FIELDS = "&_source_exclude=raw_content,canonical";
+
 	public static Timestamp getTimeStampNow() {
 		Timestamp now = new Timestamp(System.currentTimeMillis());
 		return now;
 	}
 
-	public String getListHotArticle(String from, String size, String timestamp) throws ParseException {
+	public String getListHotArticle(String from, String size, String timestamp, String connectivity) throws ParseException, UnknownHostException {
 		if (timestamp.equals("0")) {
 			Timestamp now = getTimeStampNow();
 			timestamp = String.valueOf(now.getTime());
 		}
+		if (!connectivity.equals("wifi")) { ES_FIELDS = "&_source=title,time_post,images,source"; }
 
 		Client client = ClientBuilder.newClient().property(ClientProperties.CONNECT_TIMEOUT, CONNECTION_TIMEOUT)
 				.property(ClientProperties.READ_TIMEOUT, 1000)
 				.register(JacksonJsonProvider.class);
 		WebTarget rootTarget = client
-				.target(Application.URL_ELASTICSEARCH  + "q=display:" + Application.STATUS_DISPLAY + " AND timestamp:[* TO " + timestamp + "]&from=" + from + "&size=" + size +  "&sort=time_post:desc");
+				.target(Application.URL_ELASTICSEARCH  + "q=display:" + Application.STATUS_DISPLAY
+						+ " AND timestamp:[* TO " + timestamp + "]&from=" + from + "&size=" + size
+						+  "&sort=time_post:desc" + ES_FIELDS);
 		Response response = rootTarget.request().get(); // Call get method
 
 		if (response.getStatus() == Application.RESPONE_STATAUS_OK) {
@@ -48,8 +63,8 @@ public class ArticleService {
 			JSONArray msg = new JSONArray();
 			json = (JSONObject) parser.parse(response.readEntity(JSONObject.class).toString());
 			json = (JSONObject) parser.parse(json.get("hits").toString());
-			//msg = this.getSourceImage(json);
-			msg = (JSONArray) json.get("hits");
+			msg = this.getSourceImage(json);
+			//msg = (JSONArray) json.get("hits");
 			client.close();
 			if (msg == null) {
 				throw new DataNotFoundException("Articles not found");
@@ -62,9 +77,9 @@ public class ArticleService {
 		}
 	}
 
-	public JSONArray getSourceImage(JSONObject input) {
+	public JSONArray getSourceImage(JSONObject input) throws UnknownHostException {
 		JSONArray results = (JSONArray) input.get("hits");
-		//String host = InetAddress.getLocalHost().getHostAddress();
+		String host = InetAddress.getLocalHost().getHostAddress();
 		if (results != null) {
 			for (int i =0; i < results.size(); i++) {
 				JSONObject hit = (JSONObject) results.get(i);
@@ -72,7 +87,7 @@ public class ArticleService {
 					JSONObject _source = (JSONObject) hit.get("_source");
 					String source = (String) _source.get("source");
 					if (source != null) {
-						_source.put("source_favicon", Application.HOST_NAME + "images/" + source + ".png");
+						_source.put("source_favicon", "http://" + host + "/images/" + source + ".png");
 					}
 				}
 			}
@@ -108,7 +123,7 @@ public class ArticleService {
 
 	}
 
-	public String getListArticleByCategoryId(String from, String size, String categoryId, String timestamp) throws ParseException {
+	public String getListArticleByCategoryId(String from, String size, String categoryId, String timestamp, String connectivity) throws ParseException, UnknownHostException {
 		//System.out.println("Test for cache redis:" + System.currentTimeMillis()/1000);
 		String path = "";
 		try {
@@ -116,8 +131,11 @@ public class ArticleService {
 				Timestamp now = getTimeStampNow();
 				timestamp = String.valueOf(now.getTime());
 			}
+			if (!connectivity.equals("wifi")) { ES_FIELDS = "&_source=title,time_post,images,source"; }
 			path = Application.URL_ELASTICSEARCH + "&size=" + size + "&from=" + from + "&sort=time_post:desc"
-					+ "&q=display: " + Application.STATUS_DISPLAY +  " AND category.id:" + URLEncoder.encode(categoryId, "UTF-8") + " AND timestamp:[* TO " + timestamp + "]";
+					+ "&q=display: " + Application.STATUS_DISPLAY +  " AND category.id:"
+					+ URLEncoder.encode(categoryId, "UTF-8") + " AND timestamp:[* TO " + timestamp + "]"
+					+ ES_FIELDS;
 		} catch (UnsupportedEncodingException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -149,16 +167,18 @@ public class ArticleService {
 
 	}
 
-	public String getListArticleByCategoryName(String from, String size, String categoryName, String timestamp) throws ParseException {
+	public String getListArticleByCategoryName(String from, String size, String categoryName, String timestamp, String connectivity) throws ParseException, UnknownHostException {
 		String path = "";
 		try {
 			if (timestamp.equals("0")) {
 				Timestamp now = getTimeStampNow();
 				timestamp = String.valueOf(now.getTime());
 			}
+			if (!connectivity.equals("wifi")) { ES_FIELDS = "&_source=title,time_post,images,source"; }
 			path = Application.URL_ELASTICSEARCH + "&size=" + size + "&from=" + from + "&sort=time_post:desc"
 					+ "&q=display:" + Application.STATUS_DISPLAY + " AND category.name:" +
-					URLEncoder.encode("\"" + categoryName + "\"", "UTF-8") + " AND timestamp:[* TO " + timestamp + "]";
+					URLEncoder.encode("\"" + categoryName + "\"", "UTF-8") + " AND timestamp:[* TO " + timestamp + "]"
+					+ ES_FIELDS;
 		} catch (UnsupportedEncodingException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -190,12 +210,13 @@ public class ArticleService {
 
 	}
 
-	public String getListArticleByTags(String from, String size, String tags, String timestamp) throws ParseException {
+	public String getListArticleByTags(String from, String size, String tags, String timestamp, String connectivity) throws ParseException, UnknownHostException {
 		Client client = ClientBuilder.newClient().property(ClientProperties.CONNECT_TIMEOUT, CONNECTION_TIMEOUT)
 				.property(ClientProperties.READ_TIMEOUT, 1000)
 				.register(JacksonJsonProvider.class);
+		if (!connectivity.equals("wifi")) { ES_FIELDS = "&_source=title,time_post,images,source"; }
 		WebTarget rootTarget = client
-				.target(Application.URL_ELASTICSEARCH + "&size=" + size + "&from=" + from + "&sort=time_post:desc");
+				.target(Application.URL_ELASTICSEARCH + "&size=" + size + "&from=" + from + "&sort=time_post:desc" + ES_FIELDS);
 		String jsonObject = "{\"query\" : {\"constant_score\" : { \"filter\" : {\"bool\" : { \"must\" : [ {\"terms\" : {\"tags\" : [\"" + tags + "\"]}}, {\"term\": {\"display\" :"+ Application.STATUS_DISPLAY  +"}} ] } } } } }";
 		Response response = rootTarget.request().post(Entity.json(jsonObject));
 
@@ -220,11 +241,12 @@ public class ArticleService {
 
 	}
 
-	public String getListArticlReleatedTags(String tags, String number, String timestamp) throws ParseException {
+	public String getListArticlReleatedTags(String tags, String number, String timestamp, String connectivity) throws ParseException, UnknownHostException {
 		Client client = ClientBuilder.newClient().property(ClientProperties.CONNECT_TIMEOUT, CONNECTION_TIMEOUT)
 				.property(ClientProperties.READ_TIMEOUT, 1000)
 				.register(JacksonJsonProvider.class);
-		WebTarget rootTarget = client.target(Application.URL_ELASTICSEARCH + "&size=" + number + "&sort=time_post:desc");
+		if (!connectivity.equals("wifi")) { ES_FIELDS = "&_source=title,time_post,images,source"; }
+		WebTarget rootTarget = client.target(Application.URL_ELASTICSEARCH + "&size=" + number + "&sort=time_post:desc" + ES_FIELDS);
 		String jsonObject = "{\"query\" : {\"constant_score\" : { \"filter\" : {\"bool\" : { \"must\" : [ {\"terms\" : {\"tags\" : [\"" + tags + "\"]}}, {\"term\": {\"display\" :"+ Application.STATUS_DISPLAY  +"}} ] } } } } }";
 		Response response = rootTarget.request().post(Entity.json(jsonObject));
 
@@ -249,11 +271,12 @@ public class ArticleService {
 
 	}
 
-	public String getListArticleByStringInTitle(String from, String size, String value) throws ParseException {
+	public String getListArticleByStringInTitle(String from, String size, String value, String connectivity) throws ParseException, UnknownHostException {
 		String path = "";
+		if (!connectivity.equals("wifi")) { ES_FIELDS = "&_source=title,time_post,images,source"; }
 		try {
 			path = Application.URL_ELASTICSEARCH + "&size=" + size + "&from=" + from + "&sort=time_post:desc" + "&q=display:"+Application.STATUS_DISPLAY+" AND title:"
-					+ URLEncoder.encode("\"" + value + "\"", "UTF-8");
+					+ URLEncoder.encode("\"" + value + "\"", "UTF-8") + ES_FIELDS;
 		} catch (UnsupportedEncodingException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -283,15 +306,16 @@ public class ArticleService {
 		}
 	}
 
-	public String getListArticleByStringInSource(String from, String size, String value, String timestamp) throws ParseException {
+	public String getListArticleByStringInSource(String from, String size, String value, String timestamp, String connectivity) throws ParseException, UnknownHostException {
 		String path = "";
 		try {
 			if (timestamp.equals("0")) {
 				Timestamp now = getTimeStampNow();
 				timestamp = String.valueOf(now.getTime());
 			}
+			if (!connectivity.equals("wifi")) { ES_FIELDS = "&_source=title,time_post,images,source"; }
 			path = Application.URL_ELASTICSEARCH + "&size=" + size + "&from=" + from + "&sort=time_post:desc" + "&q=display:"+Application.STATUS_DISPLAY+" AND source:"
-					+ URLEncoder.encode("\"" + value + "\"", "UTF-8") + " AND timestamp:[* TO " + timestamp + "]";
+					+ URLEncoder.encode("\"" + value + "\"", "UTF-8") + " AND timestamp:[* TO " + timestamp + "]" + ES_FIELDS;
 		} catch (UnsupportedEncodingException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -322,37 +346,25 @@ public class ArticleService {
 		}
 	}
 
-	public String getListHotTags() throws ParseException {
-		Timestamp now = getTimeStampNow();
-		Client client = ClientBuilder.newClient().property(ClientProperties.CONNECT_TIMEOUT, CONNECTION_TIMEOUT)
-				.property(ClientProperties.READ_TIMEOUT, 1000)
-				.register(JacksonJsonProvider.class);
-		String timestamp_before = String.valueOf((now.getTime() - TIMESTAMP_DAY_BEFORE) / 1000);
-		String timestamp = String.valueOf(now.getTime());
-		WebTarget rootTarget = client.target(Application.URL_ELASTICSEARCH);
-		String jsonObject = "{\"query\": { \"bool\": { \"must\": [{ \"range\": {\"time_post\" : {\"gte\" : \""+timestamp_before+"\"}}}]}},\"size\": 0, \"aggregations\": {\"hot_tags\": {\"terms\": { \"field\": \"tags\"} }}}";
-
-		Response response = rootTarget.request()
-				.post(Entity.json(jsonObject));
-
-		if (response.getStatus() == Application.RESPONE_STATAUS_OK) {
-			JSONParser parser = new JSONParser();
-			JSONObject json = new JSONObject();
-			JSONArray msg = new JSONArray();
-			json = (JSONObject) parser.parse(response.readEntity(JSONObject.class).toString());
-			json = (JSONObject) parser.parse(json.get("aggregations").toString());
-			json = (JSONObject) json.get("hot_tags");
-			msg = (JSONArray) json.get("buckets");
-			client.close();
-			if (msg == null) {
-				throw new DataNotFoundException("Tags not found");
-			} else {
-				return msg.toString().toString();
-			}
+	public ResponseEntity<Object> getListHotTags() throws ParseException {
+		String result = jedis.get(Application.REDIS_KEY);
+		if (result != "" && result != null) {
+			return ResponseEntity.ok(result);
 		} else {
-			client.close();
-			throw new DataNotFoundException("Tags not found");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
 		}
+	}
+
+	public ResponseEntity<Object> updateRedisHotTags(String input) {
+		if (input != "") {
+			try {
+				this.jedis.set(Application.REDIS_KEY, input);
+				return ResponseEntity.ok("Update success");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
 	}
 
 }

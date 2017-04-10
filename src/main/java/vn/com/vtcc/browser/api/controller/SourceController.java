291501a346a9cc4ddf05158def1c5cff96a2ffea
Nguyen Truong Giang <giangnt83@cyberspace.vn>
2017-04-10 16:46:04 +0700

    [API] Update service find logo & favicon of website

--- src/main/java/vn/com/vtcc/browser/api/controller/SourceController.java
+++ src/main/java/vn/com/vtcc/browser/api/controller/SourceController.java
@@ -1,7 +1,8 @@
 package vn.com.vtcc.browser.api.controller;
 
 import org.apache.commons.io.IOUtils;
-import org.json.simple.JSONArray;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -13,11 +14,20 @@ import sun.misc.BASE64Decoder;
 import vn.com.vtcc.browser.api.Application;
 import vn.com.vtcc.browser.api.model.Source;
 import vn.com.vtcc.browser.api.service.SourceService;
+import vn.com.vtcc.browser.api.utils.ImageUtils;
 import vn.com.vtcc.browser.api.utils.TextUtils;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 import java.util.List;
 
 /**
@@ -26,7 +36,10 @@ import java.util.List;
 @RestController
 public class SourceController {
     SourceService sourceService = new SourceService();
-    String GOOGLE_API = "https://www.google.com/search?tbm=isch&q=";
+    public static String GOOGLE_API_LOGO = "https://www.google.com/search?tbm=isch&q=logo ";
+    public static String GOOGLE_API_FAVICON = "https://www.google.com/s2/favicons?domain=";
+    public static String LOCAL_FAVICON_API = "https://icons.better-idea.org/allicons.json?url=";
+    //public static String LOCAL_FAVICON_API = "http://192.168.107.227:8181/allicons.json?url=";
 
     @CrossOrigin
     @RequestMapping(value = "/get_sources", method = RequestMethod.GET, produces = "application/json")
@@ -45,11 +58,9 @@ public class SourceController {
     public void findLogo(@RequestParam String input, HttpServletResponse response) {
         String result = "";
         JSONParser parser = new JSONParser();
-        System.setProperty("https.proxyHost", "192.168.10.34");
-        System.setProperty("https.proxyPort", "3128");
         response.setContentType(MediaType.IMAGE_PNG_VALUE);
         try {
-            String googleUrl = GOOGLE_API + input.replace(",", "") + " logo";
+            String googleUrl = GOOGLE_API_LOGO + input.replace("google.com.vn", "google.com");
             Document doc1 = Jsoup.connect(googleUrl).userAgent(Application.USER_AGENT).timeout(10 * 1000).get();
             String media = doc1.select("div#rg_s div a img").first().attr("name");
 
@@ -66,6 +77,58 @@ public class SourceController {
                 byte[] decodedBytes = decoder.decodeBuffer(result.substring(result.indexOf(",")+1));
                 InputStream in = new ByteArrayInputStream(decodedBytes);
                 IOUtils.copy(in, response.getOutputStream());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*@CrossOrigin
+    @RequestMapping(value = "/find_favicon", method = RequestMethod.GET)
+    public void findFavicon(@RequestParam String input, HttpServletResponse response) {
+        String result = "";
+        JSONParser parser = new JSONParser();
+        response.setContentType(MediaType.IMAGE_PNG_VALUE);
+        try {
+            String googleUrl = GOOGLE_API_FAVICON + input;
+            URLConnection conn = new URL(googleUrl).openConnection();
+            conn.setConnectTimeout(5000);
+            conn.setRequestProperty("User-Agent","Mozilla/5.0 ( compatible ) ");
+            conn.setReadTimeout(5000);
+
+            InputStream in = conn.getInputStream();
+            response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+            BufferedImage sourceImage = ImageIO.read(in);
+            //Image thumbnail = sourceImage.getScaledInstance(100, -1, Image.SCALE_SMOOTH);
+            BufferedImage bufferedThumbnail = ImageUtils.getScaledInstance(sourceImage,100,100 ,false);
+            //bufferedThumbnail.getGraphics().drawImage(thumbnail, 0, 0, null);
+            ImageIO.write(bufferedThumbnail, "png", response.getOutputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }*/
+
+    @CrossOrigin
+    @RequestMapping(value = "/find_favicon", method = RequestMethod.GET)
+    public void findFavicon(@RequestParam String input, HttpServletResponse response) {
+        response.setContentType(MediaType.IMAGE_PNG_VALUE);
+        try {
+            String googleUrl = LOCAL_FAVICON_API + input;
+            JSONObject json = new JSONObject(IOUtils.toString(new URL(googleUrl), Charset.forName("UTF-8")));
+            JSONArray array = json.getJSONArray("icons");
+
+            if (array != null && array.length() > 0) {
+
+                JSONObject obj = (JSONObject) array.get(0);
+                URLConnection conn = new URL(obj.getString("url")).openConnection();
+                conn.setConnectTimeout(3000);
+                conn.setRequestProperty("User-Agent","Mozilla/5.0 ( compatible ) ");
+                conn.setReadTimeout(5000);
+                InputStream in = conn.getInputStream();
+                //BufferedImage sourceImage = ImageIO.read(in);
+                IOUtils.copy(in, response.getOutputStream());
+
+                in.close();
             }
         } catch (Exception e) {
             e.printStackTrace();
---
+++ src/main/java/vn/com/vtcc/browser/api/utils/ImageUtils.java
@@ -0,0 +1,62 @@
+package vn.com.vtcc.browser.api.utils;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by giang on 08/04/2017.
+ */
+public class ImageUtils {
+    public static BufferedImage getScaledInstance(BufferedImage img,
+                                           int targetWidth,
+                                           int targetHeight,
+                                           boolean higherQuality)
+    {
+        RenderingHints rh = new RenderingHints(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        int type = (img.getTransparency() == Transparency.OPAQUE) ?
+                BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage ret = (BufferedImage)img;
+        int w, h;
+        if (higherQuality) {
+            // Use multi-step technique: start with original size, then
+            // scale down in multiple passes with drawImage()
+            // until the target size is reached
+            w = img.getWidth();
+            h = img.getHeight();
+        } else {
+            // Use one-step technique: scale directly from original
+            // size to target size with a single drawImage() call
+            w = targetWidth;
+            h = targetHeight;
+        }
+
+        do {
+            if (higherQuality && w > targetWidth) {
+                w /= 2;
+                if (w < targetWidth) {
+                    w = targetWidth;
+                }
+            }
+
+            if (higherQuality && h > targetHeight) {
+                h /= 2;
+                if (h < targetHeight) {
+                    h = targetHeight;
+                }
+            }
+
+            BufferedImage tmp = new BufferedImage(w, h, type);
+            Graphics2D g2 = tmp.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            g2.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+            g2.drawImage(ret, 0, 0, w, h, null);
+            g2.dispose();
+
+            ret = tmp;
+        } while (w != targetWidth || h != targetHeight);
+
+        return ret;
+    }
+}
